479e2139b8b8aec93216b5bfdcac66bc63c68b44
Daniel Cook <dfcook@hotmail.com>
2018-05-04 12:31:36 +0100

    Export waitForElement from dom-testing-library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 import Simulate from './Simulate'
-import { bindElementToQueries, fireEvent, wait } from 'dom-testing-library'
+import { bindElementToQueries, fireEvent, wait, waitForElement } from 'dom-testing-library'
 
 function render (TestComponent, { props = null, store = null, routes = null } = {}, configurationCb) {
   const localVue = createLocalVue()
@@ -43,4 +43,4 @@ function render (TestComponent, { props = null, store = null, routes = null } = 
   }
 }
 
-export { render, wait, fireEvent, Simulate }
+export { render, wait, waitForElement, fireEvent, Simulate }
