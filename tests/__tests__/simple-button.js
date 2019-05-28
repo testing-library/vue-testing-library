99b1e32a2f5190f5153c99c6748963e46f9568a1
Adrià Fontcuberta <afontcu@gmail.com>
2019-05-28 20:27:48 +0200

    Use events instead of callback props

--- tests/__tests__/simple-button.js
+++ tests/__tests__/simple-button.js
@@ -6,18 +6,17 @@ afterEach(cleanup)
 test('renders button with text', () => {
   const buttonText = "Click me; I'm sick"
   const { getByText } = render(SimpleButton, {
-    props: { text: buttonText, clicked: () => true }
+    props: { text: buttonText }
   })
 
   getByText(buttonText)
 })
 
-test('clicked prop is called when button is clicked', () => {
-  const clicked = jest.fn()
+test('click event is emitted when button is clicked', () => {
   const text = 'Click me'
-  const { getByText } = render(SimpleButton, {
-    props: { text, clicked }
+  const { getByText, emitted } = render(SimpleButton, {
+    props: { text }
   })
   fireEvent.click(getByText(text))
-  expect(clicked).toBeCalled()
+  expect(emitted().click).toHaveLength(1)
 })
