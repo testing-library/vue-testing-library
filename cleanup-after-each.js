5055a9c96c78cb175c111ccf00c3f34bccfa51c6
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-08-14 10:14:26 +0100

    add warning to cleanup-after-each

--- cleanup-after-each.js
+++ cleanup-after-each.js
@@ -1,1 +1,3 @@
-afterEach(require('./dist/vue-testing-library').cleanup)
+console.warn(
+  'The module `@testing-library/vue/cleanup-after-each` has been deprecated and no longer does anything (it is not needed). You no longer need to import this module and can safely remove any import or configuration which imports this module'
+)
