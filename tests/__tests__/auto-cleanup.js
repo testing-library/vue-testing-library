e1d3cb05b0e447e9654d81e8463426f155894090
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-08-14 12:20:37 +0100

    clean up the global component

--- tests/__tests__/auto-cleanup.js
+++ tests/__tests__/auto-cleanup.js
@@ -1,14 +1,13 @@
 import { render } from '@testing-library/vue'
 import '@testing-library/jest-dom/extend-expect'
 
-const HelloWorldComponent = {
-  template: `<h1>Hello World</h1>`
-}
 // This just verifies that by importing VTL in an
 // environment which supports afterEach (like jest)
 // we'll get automatic cleanup between tests.
 test('render the first component', () => {
-  const { container } = render(HelloWorldComponent)
+  const { container } = render({
+    template: `<h1>Hello World</h1>`
+  })
 
   const h1 = container.querySelector('h1')
   expect(h1).toHaveTextContent('Hello World')
