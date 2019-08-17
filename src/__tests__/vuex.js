fa85fa5fd85e7787050801dd75032a48da0554c1
Adrià Fontcuberta <afontcu@gmail.com>
2019-08-17 14:00:01 +0200

    Cleanup custom store test

--- src/__tests__/vuex.js
+++ src/__tests__/vuex.js
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom/extend-expect'
-import { cleanup, render, fireEvent } from '@testing-library/vue'
+import {cleanup, render, fireEvent} from '@testing-library/vue'
 
 import VuexTest from './components/Store/VuexTest'
-import { store } from './components/Store/store'
+import {store} from './components/Store/store'
 
 afterEach(cleanup)
 
@@ -16,19 +16,19 @@ function renderVuexTestComponent(customStore) {
   // Render the component and merge the original store and the custom one
   // provided as a parameter. This way, we can alter some behaviors of the
   // initial implementation.
-  return render(VuexTest, { store: { ...store, ...customStore } })
+  return render(VuexTest, {store: {...store, ...customStore}})
 }
 
 test('can render with vuex with defaults', async () => {
-  const { getByTestId, getByText } = renderVuexTestComponent()
+  const {getByTestId, getByText} = renderVuexTestComponent()
   await fireEvent.click(getByText('+'))
 
   expect(getByTestId('count-value')).toHaveTextContent('1')
 })
 
 test('can render with vuex with custom initial state', async () => {
-  const { getByTestId, getByText } = renderVuexTestComponent({
-    state: { count: 3 }
+  const {getByTestId, getByText} = renderVuexTestComponent({
+    state: {count: 3},
   })
   await fireEvent.click(getByText('-'))
 
@@ -36,21 +36,23 @@ test('can render with vuex with custom initial state', async () => {
 })
 
 test('can render with vuex with custom store', async () => {
-  jest.spyOn(console, 'error').mockImplementation(() => {})
-
   // This is a silly store that can never be changed.
   // eslint-disable-next-line no-shadow
-  const store = { state: { count: 1000 } }
+  const store = {
+    state: {count: 1000},
+    actions: {
+      increment: () => jest.fn(),
+      decrement: () => jest.fn(),
+    },
+  }
 
   // Notice how here we are not using the helper method, because there's no
   // need to do that.
-  const { getByTestId, getByText } = render(VuexTest, { store })
+  const {getByTestId, getByText} = render(VuexTest, {store})
 
   await fireEvent.click(getByText('+'))
   expect(getByTestId('count-value')).toHaveTextContent('1000')
 
   await fireEvent.click(getByText('-'))
   expect(getByTestId('count-value')).toHaveTextContent('1000')
-
-  expect(console.error).toHaveBeenCalledTimes(1)
 })
