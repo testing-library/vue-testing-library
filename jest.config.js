e8944840860ced68069ff9dda2644acd38ad81f8
Adrià Fontcuberta <afontcu@gmail.com>
2019-08-17 13:59:51 +0200

    Fix jest config paths

--- jest.config.js
+++ jest.config.js
@@ -8,13 +8,14 @@ module.exports = merge(config, {
     '@testing-library/vue': '<rootDir>/src/vue-testing-library.js',
   },
   coverageDirectory: './coverage',
-  collectCoverageFrom: [
-    '**/src/**/*.js',
-    '!**/src/tests/**'
-  ],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/__tests__/**'],
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules',
+    '<rootDir>/src/__tests__/components',
+  ],
 })
