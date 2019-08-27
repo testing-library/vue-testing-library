a08bc3b1e8eb91162c6741011891c205ff0dbb6f
Eduard Farre Puig <edu.farrep@gmail.com>
2019-08-27 19:06:51 +0200

    Create collapsible component

---
+++ src/__tests__/components/Collapsible.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="handleClick">Click me</button>
+    <div v-show="displayElement">
+      <p>Text</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      displayElement: false,
+    }
+  },
+  methods: {
+    handleClick(e) {
+      this.displayElement = !this.displayElement
+    },
+  },
+}
+</script>
