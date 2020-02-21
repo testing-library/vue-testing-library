cf85d1f82d8d727b4638bcb4e8dabaa14dfff8a5
Eduard Farre Puig <edu.farrep@gmail.com>
2020-02-21 17:28:22 +0100

    Implement Directive example component

---
+++ src/__tests__/components/Directive.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p v-uppercase="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'example text',
+    }
+  },
+}
+</script>
