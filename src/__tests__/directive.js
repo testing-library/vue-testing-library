cd4deb8a40b857613595afae56a9e127b997d9e0
Eduard <25011566+edufarre@users.noreply.github.com>
2020-02-22 11:08:05 +0100

    chore: Add directive test example (#120)
    * Add directive folder to test path ignore patterns to jest config
    
    * Implement v-uppercase directive
    
    * Implement Directive example component
    
    * Implement directive test

---
+++ src/__tests__/directive.js
@@ -0,0 +1,20 @@
+import {render} from '@testing-library/vue'
+import '@testing-library/jest-dom/extend-expect'
+import {uppercaseDirective} from './directives/uppercase-directive'
+import Directive from './components/Directive'
+
+// We are about to test an easy vue directive, that we have implemented,
+// named v-uppercawse.
+test('Component with a custom directive', () => {
+  // Do not forget to add the new custom directive to the render function as
+  // the third parameter.
+  const {queryByText} = render(Directive, {}, vue =>
+    vue.directive('uppercase', uppercaseDirective),
+  )
+
+  // Test that the text in lower case does not appear in the DOM
+  expect(queryByText('example text')).not.toBeInTheDocument()
+
+  // Test that the text in upper case does appear in the DOM thanks to the directive
+  expect(queryByText('EXAMPLE TEXT')).toBeInTheDocument()
+})
