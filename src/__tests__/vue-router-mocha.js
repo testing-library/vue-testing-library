58a4b5a3563e8bf73426d4263df73be5336f7a18
Alberto Gualis <agualis@gmail.com>
2020-03-17 15:15:57 +0100

    fix(mocha): allow vue-router creation using mocha (#127)
    * fix(#119): allow routes vue-router creation through routes param from both jest and mocha
    
    * add test to check vue-router require for mocha users

---
+++ src/__tests__/vue-router-mocha.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+import {render} from '@testing-library/vue'
+
+import About from './components/Router/About.vue'
+
+const routes = []
+test('uses require("vue-router").default when require("vue-router") is undefined (useful for mocha users)', () => {
+  // Test for fix https://github.com/testing-library/vue-testing-library/issues/119
+  jest.mock('vue-router', () => {
+    return undefined
+  })
+
+  expect(() => render(About, {routes})).toThrowError(
+    new TypeError("Cannot read property 'default' of undefined"),
+  )
+})
