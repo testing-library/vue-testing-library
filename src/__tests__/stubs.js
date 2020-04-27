23730bcf2b3f756cbc10bbe08e3ebdab7121658c
Adrià Fontcuberta <afontcu@gmail.com>
2020-04-27 17:48:20 +0200

    Fix stubs test

--- src/__tests__/stubs.js
+++ src/__tests__/stubs.js
@@ -3,8 +3,17 @@ import '@testing-library/jest-dom'
 import Stubs from './components/Stubs'
 
 test('Form contains search button', () => {
+  const DirectiveMock = {
+    template: '<p>fake template</p>',
+  }
+
   const {getByText} = render(Stubs, {
-    stubs: ['FontAwesomeIcon'],
+    global: {
+      stubs: {
+        directive: DirectiveMock,
+      },
+    },
   })
+
   expect(getByText('Search now')).toBeInTheDocument()
 })
