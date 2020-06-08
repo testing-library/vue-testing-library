650ff5aebc3c4472a6ceb7954b9954d44b7b1365
Adrià Fontcuberta <afontcu@gmail.com>
2020-06-08 16:57:29 +0200

    Comment out test that rely on vue2

--- src/__tests__/directive.js
+++ src/__tests__/directive.js
@@ -1,20 +1,20 @@
-import {render} from '@testing-library/vue'
-import '@testing-library/jest-dom'
-import {uppercaseDirective} from './directives/uppercase-directive'
-import Directive from './components/Directive'
+test.todo('Your test suite must contain at least one test.')
 
-// We are about to test an easy vue directive, that we have implemented,
-// named v-uppercawse.
-test('Component with a custom directive', () => {
-  // Do not forget to add the new custom directive to the render function as
-  // the third parameter.
-  const {queryByText, getByText} = render(Directive, {}, vue =>
-    vue.directive('uppercase', uppercaseDirective),
-  )
+// import {render} from '@testing-library/vue'
+// import '@testing-library/jest-dom'
+// import {uppercaseDirective} from './directives/uppercase-directive'
+// import Directive from './components/Directive'
 
-  // Test that the text in lower case does not appear in the DOM
-  expect(queryByText('example text')).not.toBeInTheDocument()
+// test('Component with a custom directive', () => {
+//   // Do not forget to add the new custom directive to the render function as
+//   // the third parameter.
+//   const {queryByText, getByText} = render(Directive, {}, vue =>
+//     vue.directive('uppercase', uppercaseDirective),
+//   )
 
-  // Test that the text in upper case does appear in the DOM thanks to the directive
-  expect(getByText('EXAMPLE TEXT')).toBeInTheDocument()
-})
+//   // Test that the text in lower case does not appear in the DOM
+//   expect(queryByText('example text')).not.toBeInTheDocument()
+
+//   // Test that the text in upper case does appear in the DOM thanks to the directive
+//   expect(getByText('EXAMPLE TEXT')).toBeInTheDocument()
+// })
--- src/__tests__/slots.js
+++ src/__tests__/slots.js
@@ -1,32 +1,34 @@
-import '@testing-library/jest-dom'
-import {render} from '@testing-library/vue'
-import Card from './components/Card'
+test.todo('Your test suite must contain at least one test.')
 
-// In this test file we demo how to test a component with slots and a scoped slot.
+// import '@testing-library/jest-dom'
+// import {render} from '@testing-library/vue'
+// import Card from './components/Card'
 
-// Usage is the same as Vue Test Utils, since slots and scopedSlots
-// in the render options are directly passed through to the Utils mount().
-// For more, see: https://vue-test-utils.vuejs.org/api/options.html#slots
-test('Card component', () => {
-  const {getByText, queryByText} = render(Card, {
-    slots: {
-      header: '<h1>HEADER</h1>',
-      footer: '<div>FOOTER</div>',
-    },
-    scopedSlots: {
-      default: '<p>Yay! {{props.content}}</p>',
-    },
-  })
+// // In this test file we demo how to test a component with slots and a scoped slot.
 
-  // The default slot should render the template above with the scoped prop "content".
-  expect(getByText('Yay! Scoped content!')).toBeInTheDocument()
+// // Usage is the same as Vue Test Utils, since slots and scopedSlots
+// // in the render options are directly passed through to the Utils mount().
+// // For more, see: https://vue-test-utils.vuejs.org/api/options.html#slots
+// test('Card component', () => {
+//   const {getByText, queryByText} = render(Card, {
+//     slots: {
+//       header: '<h1>HEADER</h1>',
+//       footer: '<div>FOOTER</div>',
+//     },
+//     scopedSlots: {
+//       default: '<p>Yay! {{props.content}}</p>',
+//     },
+//   })
 
-  // Instead of the default slot's fallback content.
-  expect(
-    queryByText('Nothing used the Scoped content!'),
-  ).not.toBeInTheDocument()
+//   // The default slot should render the template above with the scoped prop "content".
+//   expect(getByText('Yay! Scoped content!')).toBeInTheDocument()
 
-  // And the header and footer slots should be rendered with the given templates.
-  expect(getByText('HEADER')).toBeInTheDocument()
-  expect(getByText('FOOTER')).toBeInTheDocument()
-})
+//   // Instead of the default slot's fallback content.
+//   expect(
+//     queryByText('Nothing used the Scoped content!'),
+//   ).not.toBeInTheDocument()
+
+//   // And the header and footer slots should be rendered with the given templates.
+//   expect(getByText('HEADER')).toBeInTheDocument()
+//   expect(getByText('FOOTER')).toBeInTheDocument()
+// })
