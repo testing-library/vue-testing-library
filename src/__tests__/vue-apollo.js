76cdc70c340e90563963c001499f37ad022370ad
Alexander Plavinski <hello@cilice.me>
2020-08-27 10:09:43 +0200

    docs(vue-apollo): rework example to use msw (#154)

--- src/__tests__/vue-apollo.js
+++ src/__tests__/vue-apollo.js
@@ -1,45 +1,56 @@
-// Caution!
-// This approach only works with apollo-client 2.x and vue-apollo 3.x
-
 import '@testing-library/jest-dom'
+import fetch from 'isomorphic-unfetch'
 import {render, fireEvent, screen} from '@testing-library/vue'
 import VueApollo from 'vue-apollo'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import ApolloClient from 'apollo-boost'
 
 // Since vue-apollo doesn't provides a MockProvider for Vue,
-// you need to use some kind of a mocking client for Apollo.
+// you need to use some kind of mocks for the queries.
 
-// If you decide to use `mock-apollo-client`,
-// have a look at the documentation at
-// https://github.com/Mike-Gibson/mock-apollo-client
-import {createMockClient} from 'mock-apollo-client'
+// We recommend using Mock Service Worker library to declaratively mock API communication
+// in your tests instead of stubbing window.fetch, or relying on third-party adapters.
+
+import {setupServer} from 'msw/node'
+import {graphql} from 'msw'
 
 import Component from './components/VueApollo.vue'
-import {userQuery, updateUserMutation} from './components/VueApollo/queries'
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:3020/graphql',
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+  fetch,
+})
+
+const server = setupServer(
+  ...[
+    graphql.mutation('updateUser', (req, res, ctx) => {
+      const {variables} = req
+
+      return res(
+        ctx.data({
+          updateUser: {id: variables.input.id, email: variables.input.email},
+        }),
+      )
+    }),
+    graphql.query('User', (req, res, ctx) => {
+      return res(ctx.data({user: {id: '1', email: 'alice@example.com'}}))
+    }),
+  ],
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 test('mocking queries and mutations', async () => {
-  const mockClient = createMockClient()
-
-  mockClient.setRequestHandler(userQuery, () =>
-    Promise.resolve({
-      data: {user: {id: '1', email: 'alice@example.com'}},
-    }),
-  )
-
-  mockClient.setRequestHandler(updateUserMutation, variables =>
-    Promise.resolve({
-      data: {
-        updateUser: {id: variables.input.id, email: variables.input.email},
-      },
-    }),
-  )
-
   render(Component, {props: {id: '1'}}, localVue => {
     localVue.use(VueApollo)
 
     return {
-      apolloProvider: new VueApollo({
-        defaultClient: mockClient,
-      }),
+      apolloProvider: new VueApollo({defaultClient: apolloClient}),
     }
   })
 
