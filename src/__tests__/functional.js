347a30bdeb976171f227979e30ce74b5dc50cf35
Adrià Fontcuberta <afontcu@gmail.com>
2020-10-09 18:28:52 +0200

    Fix functional tests

--- src/__tests__/functional.js
+++ src/__tests__/functional.js
@@ -1,26 +1,17 @@
-test.todo('Your test suite must contain at least one test.')
-// import {render} from '@testing-library/vue'
-// import '@testing-library/jest-dom'
-// import FunctionalSFC from './components/FunctionalSFC'
+import {render} from '@testing-library/vue'
+import '@testing-library/jest-dom'
+import {h} from 'vue'
 
-// import {render} from '@testing-library/vue'
-// import FunctionalSFC from './components/FunctionalSFC'
+// From docs: Performance gains from 2.x for functional components are now
+// negligible in 3.x, so we recommend just using stateful components.
 
-// const Functional = {
-//   functional: true,
-//   render(createElement) {
-//     return createElement('p', null, 'Hi!')
-//   },
-// }
+// eslint-disable-next-line no-unused-vars
+const Functional = (props, context) => {
+  return h('div', {}, 'Hi!')
+}
 
-// test('renders functional component', () => {
-//   const {getByText} = render(Functional)
+test('renders functional component', () => {
+  const {getByText} = render(Functional)
 
-//   expect(getByText('Hi!')).toBeInTheDocument()
-// })
-
-// test('renders functional SFC component', () => {
-//   const {getByText} = render(FunctionalSFC)
-
-//   expect(getByText('Hi!')).toBeInTheDocument()
-// })
+  expect(getByText('Hi!')).toBeInTheDocument()
+})
