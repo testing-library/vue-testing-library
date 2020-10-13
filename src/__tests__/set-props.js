6e490c0b6754545c6a4bdebe6389e73d992de91d
Adrià Fontcuberta <afontcu@gmail.com>
2020-10-13 23:45:35 +0200

    Add composition API example

--- src/__tests__/set-props.js
+++ src/__tests__/set-props.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
+import {defineComponent, h, computed} from 'vue'
 import {render} from '@testing-library/vue'
-import NumberDisplay from './components/NumberDisplay.vue'
+import NumberDisplay from './components/NumberDisplay'
 
 // It'd probably be better if you test the component that's doing the prop
 // updating to ensure that the props are being updated correctly.
@@ -20,4 +21,31 @@ test('calling render with the same component but different props does not remoun
   // Assert that, even after updating props, the component hasn't remounted,
   // meaning we are testing the same component instance we rendered initially.
   expect(getByTestId('instance-id')).toHaveTextContent('1')
+})
+
+test('works with composition API', async () => {
+  const Component = defineComponent({
+    props: {
+      foo: {type: String, default: 'foo'},
+    },
+    setup(props) {
+      const foobar = computed(() => `${props.foo}-bar`)
+      return () =>
+        h(
+          'div',
+          {'data-testid': 'node'},
+          `Foo is: ${props.foo}. Foobar is: ${foobar.value}`,
+        )
+    },
+  })
+
+  const {setProps, getByTestId} = render(Component)
+
+  const node = getByTestId('node')
+
+  expect(node).toHaveTextContent('Foo is: foo. Foobar is: foo-bar')
+
+  await setProps({foo: 'qux'})
+
+  expect(node).toHaveTextContent('Foo is: qux. Foobar is: qux-bar')
 })
