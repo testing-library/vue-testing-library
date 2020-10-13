1496dd1fb7ada3061782926d6b9bdb76cdba00f4
Adrià Fontcuberta <afontcu@gmail.com>
2020-10-13 16:14:31 +0200

    Fix vee-validate tests

--- src/__tests__/validate-plugin.js
+++ src/__tests__/validate-plugin.js
@@ -1,31 +1,22 @@
-test.todo('Your test suite must contain at least one test.')
+import '@testing-library/jest-dom'
 
-// // Notice this example is using vee-validate v2.X
-// import VeeValidate from 'vee-validate'
-// import '@testing-library/jest-dom'
+import {render, fireEvent} from '@testing-library/vue'
+import VeeValidate from './components/Validate'
 
-// import {render, fireEvent} from '@testing-library/vue'
-// import Validate from './components/Validate'
+test('can validate using plugin', async () => {
+  const {findByText, getByRole, getByTestId} = render(VeeValidate)
 
-// test('can validate using plugin', async () => {
-//   // The third argument of `render` is a callback function that receives the
-//   // Vue instance as a parameter. This way, we can register plugins such as
-//   // VeeValidate.
-//   const {getByPlaceholderText, queryByTestId, getByTestId} = render(
-//     Validate,
-//     {},
-//     vue => vue.use(VeeValidate, {events: 'blur'}),
-//   )
+  // Assert error messages are not in the DOM when rendering the component.
+  expect(getByTestId('error-message')).toBeEmptyDOMElement()
 
-//   // Assert error messages are not in the DOM when rendering the component.
-//   expect(queryByTestId('username-errors')).toBeNull()
+  const emailInput = getByRole('textbox')
 
-//   const usernameInput = getByPlaceholderText('Username...')
-//   await fireEvent.touch(usernameInput)
+  await fireEvent.touch(emailInput)
 
-//   // After "touching" the input (focusing and blurring), validation error
-//   // should appear.
-//   expect(getByTestId('username-errors')).toHaveTextContent(
-//     /the username field is required/i,
-//   )
-// })
+  await findByText('This field is required')
+
+  await fireEvent.update(emailInput, 'an invalid email')
+  await fireEvent.blur(emailInput)
+
+  await findByText('This field must be a valid email')
+})
