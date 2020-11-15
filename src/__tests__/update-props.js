95cc44500cb14a1dca9055d5abe83d7544ab6645
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2020-11-15 03:11:12 -0500

    fix: allow multiple calling of updateProps (#170)
    * Changed updateProps to return the Promise created from
    wrapper.setProps.
    * Updated the `updateProps` test to make sure the function can be called
    multiple times without producing errors.

--- src/__tests__/update-props.js
+++ src/__tests__/update-props.js
@@ -14,8 +14,10 @@ test('calling render with the same component but different props does not remoun
   expect(getByTestId('number-display')).toHaveTextContent('1')
 
   await updateProps({number: 2})
-
   expect(getByTestId('number-display')).toHaveTextContent('2')
+
+  await updateProps({number: 3})
+  expect(getByTestId('number-display')).toHaveTextContent('3')
 
   // Assert that, even after updating props, the component hasn't remounted,
   // meaning we are testing the same component instance we rendered initially.
