88a52c7a58fd6c8502934fce4581c2149a7e1ad3
Adrià Fontcuberta <afontcu@gmail.com>
2021-02-17 20:18:37 +0100

    Split index into focused files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {cleanup} from './render'
+
+// If we're running in a test runner that supports afterEach then we'll
+// automatically run cleanup after each test.
+// This ensures that tests run in isolation from each other.
+// If you don't like this, set the VTL_SKIP_AUTO_CLEANUP variable to 'true'.
+if (typeof afterEach === 'function' && !process.env.VTL_SKIP_AUTO_CLEANUP) {
+  afterEach(() => {
+    cleanup()
+  })
+}
+
+export * from '@testing-library/dom'
+export {cleanup, render} from './render'
+export {fireEvent} from './fire-event'
