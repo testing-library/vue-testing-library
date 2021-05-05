3b4a564ba0672c81fab24a43403c852d66c79d7d
Sander Tegelaar <info@mesander.com>
2021-05-05 22:10:11 +0200

    fix(types): expose RenderOptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -42,7 +42,7 @@ interface VueTestingLibraryRenderOptions {
   container?: Element
   baseElement?: Element
 }
-type RenderOptions = VueTestingLibraryRenderOptions & VueTestUtilsRenderOptions
+export type RenderOptions = VueTestingLibraryRenderOptions & VueTestUtilsRenderOptions
 
 export function render(
   TestComponent: any, // this makes me sad :sob:
