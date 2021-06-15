631527d2371b71cbd5d08a990d070027e209096b
Adrià Fontcuberta <afontcu@gmail.com>
2021-06-15 08:59:39 +0200

    chore: fix element-plus test

--- src/__tests__/element-plus.js
+++ src/__tests__/element-plus.js
@@ -1,37 +1,19 @@
 import ElementPlus from 'element-plus'
 import {defineComponent} from 'vue'
-import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
-import {render, screen, waitFor} from '..'
+import {render, screen} from '..'
 
 const Component = defineComponent({
   template: `
-    <el-popover trigger="click" content="this is content">
-      <template #reference>
-        <el-button>Click to activate</el-button>
-      </template>
-    </el-popover>
-  `,
+    <el-alert title="success alert" type="success" />`,
 })
 
-test('Stubs out a component', async () => {
+test('Renders component from third-party library', async () => {
   render(Component, {
     global: {
       plugins: [ElementPlus],
     },
   })
 
-  const button = screen.getByRole('button')
-  const getContent = () => screen.getByText('this is content')
-
-  expect(getContent()).toBeInTheDocument()
-  expect(getContent()).not.toBeVisible()
-
-  userEvent.click(button)
-
-  await waitFor(() => expect(getContent()).toBeVisible())
-
-  userEvent.click(button)
-
-  await waitFor(() => expect(getContent()).not.toBeVisible())
+  expect(screen.getByRole('alert')).toHaveTextContent('success alert')
 })
