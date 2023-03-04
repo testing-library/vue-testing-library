23cf8374557e3a603aa0f1c95286760f0a04ad95
Adrià Fontcuberta <adria.fontcuberta@holaluz.com>
2023-03-04 09:19:58 +0100

    Bump node and DTL versions

--- src/__tests__/about-vue-router-mocha.js
+++ src/__tests__/about-vue-router-mocha.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom'
 import {render} from '@testing-library/vue'
-import semver from 'semver'
 
 import About from './components/Router/About.vue'
 
@@ -12,10 +11,6 @@ test('uses require("vue-router").default when require("vue-router") is undefined
   })
 
   expect(() => render(About, {routes})).toThrowError(
-    new TypeError(
-      semver.gte(process.version, '16.0.0')
-        ? "Cannot read properties of undefined (reading 'default')"
-        : "Cannot read property 'default' of undefined",
-    ),
+    new TypeError("Cannot read property 'default' of undefined"),
   )
 })
