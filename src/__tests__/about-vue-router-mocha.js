dede1996241133e8a75aebb27c02bd70c55c54fa
Adrià Fontcuberta <adria.fontcuberta@holaluz.com>
2023-03-07 09:01:31 +0100

    fix: Update workflow to release 5.x

--- src/__tests__/about-vue-router-mocha.js
+++ src/__tests__/about-vue-router-mocha.js
@@ -1,22 +1,24 @@
 /* eslint-disable jest/no-conditional-in-test */
+/* eslint-disable jest/no-conditional-expect */
+
 import '@testing-library/jest-dom'
 import {render} from '@testing-library/vue'
-import semver from 'semver'
 
 import About from './components/Router/About.vue'
 
 const routes = []
 test('uses require("vue-router").default when require("vue-router") is undefined (useful for mocha users)', () => {
+  expect.assertions(2)
+
   // Test for fix https://github.com/testing-library/vue-testing-library/issues/119
   jest.mock('vue-router', () => {
     return undefined
   })
 
-  expect(() => render(About, {routes})).toThrowError(
-    new TypeError(
-      semver.gte(process.version, '16.0.0')
-        ? "Cannot read properties of undefined (reading 'default')"
-        : "Cannot read property 'default' of undefined",
-    ),
-  )
+  try {
+    render(About, {routes})
+  } catch (error) {
+    expect(error).toBeInstanceOf(TypeError)
+    expect(error.message).toEqual(expect.stringContaining('default'))
+  }
 })
