4025d2a34ef760c244cb49edfa73647d1ad8f5ed
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2023-11-17 10:57:14 -0500

    style: Combine Tests Related to Test Running Env

--- src/__tests__/auto-cleanup-global-env.js
+++ src/__tests__/auto-cleanup-global-env.js
@@ -1,3 +1,13 @@
+beforeEach(jest.restoreAllMocks)
+
+test('Testing Utilities with global access to `afterEach()` will not log warnings', () => {
+  jest.spyOn(console, 'warn')
+  global.afterEach = () => {}
+
+  require('..')
+  expect(console.warn).not.toHaveBeenCalled()
+})
+
 test('Testing Utilities lacking a global `afterEach` function will log a warning ONCE', () => {
   delete global.afterEach
   const warn = jest.spyOn(console, 'warn')
--- src/__tests__/auto-cleanup-vitest-globals.js
+++
@@ -1,7 +0,0 @@
-test('Testing Utilities with global access to `afterEach()` will not log warnings', () => {
-  jest.spyOn(console, 'warn')
-  global.afterEach = () => {}
-
-  require('..')
-  expect(console.warn).not.toHaveBeenCalled()
-})
