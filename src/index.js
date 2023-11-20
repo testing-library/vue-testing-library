1bbeeb404fcf461120548d7a9ae6817f3f184a73
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2023-11-20 10:38:57 -0500

    fix: Revert Errors Regarding Missing Global Hooks (#310)
    Please bear in mind that after 8.0.1, v7 and v8 share the same behavior as the main difference between versions has been erased. So everyone should be able to jump from v7 to v8 without experiencing any difference.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ if (typeof afterEach === 'function' && !process.env.VTL_SKIP_AUTO_CLEANUP) {
   afterEach(() => {
     cleanup()
   })
-} else if (process.env.VITEST === 'true') {
-  throw new Error(
-    "You are using vitest without globals, this way we can't run cleanup after each test.\n" +
-      "See https://testing-library.com/docs/vue-testing-library/setup for details or set the VTL_SKIP_AUTO_CLEANUP variable to 'true'",
-  )
 }
 
 export * from '@testing-library/dom'
